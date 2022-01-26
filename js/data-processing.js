2bd24c69995f6c2c0b70170b57e9f0f3d1e33239
Ermiaas <ermiaasm@gmail.com>
2022-01-25 20:28:33 -0800

    push

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,22 +1,76 @@
  //use location object to access querystring (address bar)
+ function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  return str.join(' ');
+}
+titleCase("");
  const queryString = window.location.search;
+ let myData = ''; //will store data for output
+ let myCart = '' //will store cart details
+ let myTotal = 0; // will store total cost
     
- //output to console    
- console.log(queryString);
-     
- //separate querystring parameters
- const urlParams = new URLSearchParams(queryString);
- 
- let myData = '';
- urlParams.forEach(function(value, key) {
+ if(queryString !=""){ //process data
+      //output to console    
+  console.log(queryString);
 
-   //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+  //separate querystring parameters
+  const urlParams = new URLSearchParams(queryString);
+  
+  let myData = '';
+  urlParams.forEach(function(value, key) {
+
+    if(key =="Cart") { //Process Cart
+      switch(value){
+          case  "Widget":
+              myCart += "Widget: $3.99 <br />";
+              myTotal += 3.99;
+          break;
+
+          case  "Sprocket":
+              myCart += "Sprocket: $5.99 <br />";
+              myTotal += 5.99;
+          break;
+
+          case  "Thingy":
+              myCart += "Thingy: $1.99 <br />";
+              myTotal += 1.99;
+          
+      }
+
+    }else{//build shipping info
+          //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+          switch(key){  
+            case "First_Name":
+            case "Last_Name":
+            case "Address":
+            case "City":
+
+              value = titleCase(value);
+
+              break;
+          }
+          key = key.split("_").join("");
+
+    }
+
+
     key = key.split("_").join(" ");
+          myData += `<p>${key}: ${value} </p>`;
+          //console.log(value, key);
+    });
 
-   myData += `<p>${key}: ${value} </p>`;
-     //console.log(value, key);
-   });
+    myCart = `
+    <p><b>Cart Contents</b></p>
+    <p>${myCart}</p>
+    <p>Total: $${myTotal}</p>
+`;
 
-   myData += `<p><a href =".indexHTML" >Clear</a></p>`
-   
-   document.getElementById("output").innerHTML = myData;
+  myData = myCart + "<p><b>Shipping Information </b></p>" + myData;
+  myData += '<p><a href="index.html">CLEAR</a></p>';
+  document.getElementById("output").innerHTML = myData;
+}
+
+  
